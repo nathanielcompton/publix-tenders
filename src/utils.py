328814a7fb023c951915839d6ecedca1bddb03b5
Nathaniel Compton <nathanielcompton@gmail.com>
2019-11-25 21:30:15 -0500

    Remove unused class

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,13 @@
 import re
 import requests
 from bs4 import BeautifulSoup, Comment
-
 from settings import TENDERS_SALE_URL, ONSALE_STRING
 
-class Tenders:
-
-	def scrape_tendies_meme_site() -> bool:
-		res = requests.get(TENDERS_SALE_URL)
-		soup = BeautifulSoup(res.text, "html.parser")
-		# comments = list of stringified code comments from the soup
-		comments = str(soup.find_all(string=lambda text: isinstance(text, Comment)))
-		if re.search(ONSALE_STRING, comments):
-			return True
-		return False
+def scrape_tendies_meme_site() -> bool:
+	res = requests.get(TENDERS_SALE_URL)
+	soup = BeautifulSoup(res.text, "html.parser")
+	# comments = list of stringified code comments from the soup
+	comments = str(soup.find_all(string=lambda text: isinstance(text, Comment)))
+	if re.search(ONSALE_STRING, comments):
+		return True
+	return False
